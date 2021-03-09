bec75730fa81bf69ff8aedb7aa19a33a59b87aad
Aaron Lindsey <alindsey@vmware.com>
2021-03-08 18:00:15 -0800

    WIP: remove files accidentally checked in

--- start-cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./geode-assembly/build/install/apache-geode/bin/gfsh \
-  -e "start locator --name=locator --security-properties-file=security.properties" \
-  -e "start server --name=server --security-properties-file=security.properties"
